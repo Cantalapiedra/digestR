f9794c3b5cc9e6e44798559ba23f03a095a7ebcb
Cantalapiedra <cpcantalapiedra@gmail.com>
2017-02-20 17:20:36 +0100

    matchPattern

--- digest.R
+++ digest.R
@@ -32,7 +32,7 @@ if (length(args)==2) {
 
 # Read enzymes from a configuration file
 
-df_enz = read.table(enz_file, header=FALSE, sep = "\t", col.names = c("Enzyme", "Target"))
+df_enz = read.table(enz_file, header=FALSE, sep = "\t", col.names = c("name", "target"))
 
 write.table(df_enz, stdout())
 
@@ -40,33 +40,26 @@ write.table(df_enz, stdout())
 
 genome <- readDNAStringSet(genome_file, "fasta")
 
-head(genome)
+genome
+names(genome)
 
 # Identify the MspI recongnition sites for each chromosomal entry
 # Generate a dataframe with the length of MspI digested fragments
 mdf=data.frame();
-for (i in seq_along(Mmusculus)){
-  #print(paste("Processing ",seqnames(Mmusculus)[i], sep=""))
-  for (j in seq_along(Enzymes)){
-    enz_target = Enzymes[i]$target
-    m<-matchPattern(enz_target, Mmusculus[[i]])
-    
-    enz_target_len = seq_len(enz_target)
-    starts<-start(gaps(m))-enz_target_len
-    ends<-end(gaps(m))
-    
-    enz_name = Enzymes[i]$name
-    temp_df<-data.frame(start=starts,end=ends,chr=seqnames(Mmusculus)[i],enzyme=enz_name) #actually end = ends
-    temp_df$start<-replace(temp_df$start, temp_df$start == -3, 0)
-    temp_df<-temp_df[c("chr","start","end","enzyme")]
-    mdf<-rbind(mdf,temp_df)
+
+f_enz <- function(enz){
+  write(paste("\t", enz["name"], "\t", enz["target"]), stdout())
+  count <- vcountPattern(enz["target"], genome)
+  count
+  write(count, stdout())
+  match <- vmatchPattern(enz["target"], genome)
+  match
+  write.table(match, stdout())
 }
 
-# Extract the digested fragment length
-# Keep only the fragments in the range of 40-600 bp
-mdf$width=mdf$end-mdf$start
-ml<-mdf[mdf$width>39&mdf$width<601,]
-counts<-ddply(ml,.(width), nrow)
-# Create a plot of the frequency of the fragment lengths (y-axis is logarithmic)
-p<-ggplot(counts,aes(x=width, y=V1))+geom_line()
-p+scale_y_continuous(trans=log2_trans())
+write("START", stdout())
+apply(df_enz, 1, f_enz)
+
+write("END", stdout())
+
+## END
