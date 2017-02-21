5a18f7d5d1e81b37c226738314874c2f62c4c815
Cantalapiedra <cpcantalapiedra@gmail.com>
2017-02-21 13:07:30 +0100

    beta0.1

--- digest.R
+++ digest.R
@@ -3,16 +3,8 @@
 ## 2017 CPCantalapiedra
 ## based on https://www.r-bloggers.com/restriction-digestion-of-eukaryotic-genomes-in-r/
 
-### Load the needed libraries
-# Biostrings is needed for pattern identification
-# plyr and reshape2 are needed for manipulating the data format
-
 library(Biostrings)
-#library(BSgenome.Mmusculus.UCSC.mm10) # HOW TO LOAD THE Hvulg genome
-library(plyr)
-library(ggplot2)
-library(reshape2)
-library(scales)
+library(data.table)
 
 # Read command line arguments
 
@@ -23,40 +15,60 @@ if (length(args)==2) {
   enz_file = args[1]
   genome_file = args[2]
   
-  write(enz_file, stdout())
-  write(genome_file, stdout())
-  
 } else {
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
 }
 
 # Read enzymes from a configuration file
 
+write(paste("Parsing enzymes configuration file:", enz_file), stderr())
+
 df_enz = read.table(enz_file, header=FALSE, sep = "\t", col.names = c("name", "target"))
 
-write.table(df_enz, stdout())
+write.table(df_enz, file = stderr(), row.names = FALSE, quote = FALSE, sep = "\t")
 
 # Read genome from file
 
+write(paste("\nParsing fasta input file:", genome_file), stderr())
 genome <- readDNAStringSet(genome_file, "fasta")
 
-genome
+write(paste("Num. sequences read:", length(genome)), stderr())
+write(paste("\t", names(genome), "of length:", width(genome)), stderr())
 
-# Identify the MspI recongnition sites for each chromosomal entry
-# Generate a dataframe with the length of MspI digested fragments
-mdf=data.frame();
-
+# Function to find matches for each enzyme
+# In theory, this could be done with matchPDict also avoiding
+# the explicit loop (apply function)
 f_enz <- function(enz){
-  write(paste("\t", enz["name"], "\t", enz["target"]), stdout())
-  #count <- vcountPattern(enz["target"], genome)
-  #write(count, stdout())
+  
   match <- vmatchPattern(enz["target"], genome, fix=FALSE)
-  write.table(match, stdout())
+  ranges <- unlist(match)
+  
+  write(paste("\t", enz["name"], " - ", enz["target"], " --> Matches found: ", length(ranges), sep = ""), file = stderr())
+  
+  if (length(ranges)>0) {
+    ranges <- as.data.frame(ranges)
+    ranges <- cbind(ranges, enzyme=enz["name"], row.names = NULL)
+    ranges <- ranges[,c("names", "start", "end", "enzyme")]
+    #write.table(ranges, stderr())
+  } else {
+    ranges <- as.data.frame(ranges)
+    ranges <- cbind(ranges, enzyme=numeric(0), row.names = NULL)
+    ranges <- ranges[,c("names", "start", "end", "enzyme")]
+  }
+  
+  return(ranges)
 }
 
-write("START", stdout())
-apply(df_enz, 1, f_enz)
+write("\nstart cutting...", stderr())
 
-write("END", stdout())
+all.ranges <- do.call(rbind, apply(df_enz, 1, f_enz))
+all.ranges = data.table(all.ranges)
+setorder(all.ranges, names, start, end, enzyme)
+
+write("writing results...", stderr())
+
+write.table(all.ranges, file = stdout(), row.names=FALSE, quote=FALSE, sep="\t")
+
+write("finished.", stderr())
 
 ## END
